989ef69a2cf5e157851d21d62bf8589dc70403b0
Matt <aboutmattlaw@gmail.com>
2021-12-17 10:41:58 -0500

    adds buttons for specific stops, clicking button gets new times and replaces old times

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function getBus() {
+function getBus(stopcode) {
   fetch(
-    'https://warm-harbor-67209.herokuapp.com/https://bustime.mta.info/api/siri/stop-monitoring.json?key=cb9d18ca-68e1-43d3-9f36-d49533819a07&OperatorRef=MTA&MonitoringRef=308209&LineRef=MTA%20NYCT_B63',
+    `https://warm-harbor-67209.herokuapp.com/https://bustime.mta.info/api/siri/stop-monitoring.json?key=cb9d18ca-68e1-43d3-9f36-d49533819a07&OperatorRef=MTA&MonitoringRef=${stopcode}`,
   )
     .then((response) => response.json())
     .then((data) => showBus(data))
@@ -8,30 +8,29 @@ function getBus() {
 
 function showBus(json) {
   const main = document.querySelector('main')
+  main.innerHTML = ""
+
   json.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit.map(
     (visit) => {
       const distance = document.createElement('h2')
       const arrival = document.createElement('h2')
 
-      
-      distance.innerHTML =
-        visit.MonitoredVehicleJourney.MonitoredCall.Extensions.Distances.PresentableDistance 
-      
-        arrival.innerHTML =
-        visit.MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime
+      distance.textContent =
+        visit.MonitoredVehicleJourney.MonitoredCall.Extensions.Distances.PresentableDistance
 
-         
-                  
-        
+      var time = visit.MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime
+      // var newTime = Date(time)
 
-        main.appendChild(distance)
-        main.appendChild(arrival)
+      arrival.textContent = time
 
-      
+
+      main.appendChild(distance)
+      main.appendChild(arrival)
+
     },
   )
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  getBus()
-})
+// document.addEventListener('DOMContentLoaded', function () {
+//   getBus()
+// })
